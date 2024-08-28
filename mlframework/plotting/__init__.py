fecba1af9eb07541e4db53a027fe87a4408a8243
Kristian Ehlert <kristian.ehlert@gmail.com>
2024-08-28 22:26:12 +0200

    introduce plots

---
+++ mlframework/plotting/__init__.py
@@ -0,0 +1,5 @@
+import mlframework.plotting.analysis
+import mlframework.plotting.figures
+import mlframework.plotting.histograms
+import mlframework.plotting.timeseries
+import mlframework.plotting.utils_plotting
