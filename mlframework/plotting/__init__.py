837118c92d14c0dcbf15ac6151c5da402b71f040
Kristian Ehlert <kristian.ehlert@4-cast.de>
2024-12-19 16:35:15 +0100

    update to uv

--- mlframework/plotting/__init__.py
+++ mlframework/plotting/__init__.py
@@ -6,3 +6,4 @@ import mlframework.plotting.histograms
 import mlframework.plotting.lines
 import mlframework.plotting.timeseries
 import mlframework.plotting.value_counts
+from mlframework.plotting.histograms import plot_histogram, plot_histogram_2d
--- mlframework/split.py
+++ mlframework/split.py
@@ -26,7 +26,7 @@ def split_dataset(
             f"{validate_size=} + {test_size=} + {train_size=} != {validate_size + test_size + train_size}"
         )
 
-    train_size, validate_size, test_size = (
+    _train_size, _validate_size, _test_size = (
         round(train_size, 6),
         round(validate_size, 6),
         round(test_size, 6),
@@ -35,23 +35,22 @@ def split_dataset(
         stratify = df[stratify_column]
     train, validate_test = train_test_split(
         df,
-        train_size=train_size,
-        test_size=test_size + validate_size,
+        train_size=_train_size,
+        test_size=round(1 - _train_size, 6),
         random_state=random_state,
         stratify=stratify,
         shuffle=shuffle,
     )
-    if test_size != 0:
-        validate_size, test_size = validate_size / (
-            validate_size + test_size
-        ), test_size / (validate_size + test_size)
-        validate_size, test_size = round(validate_size, 6), round(test_size, 6)
+    if _test_size != 0:
+        _validate_size = _validate_size / (_validate_size + _test_size)
+        _test_size = 1 - _validate_size
+        _validate_size, _test_size = round(_validate_size, 6), round(_test_size, 6)
         if stratify_column is not None:
             stratify = validate_test[stratify_column]
         validate, test = train_test_split(
             validate_test,
-            train_size=validate_size,
-            test_size=test_size,
+            train_size=_validate_size,
+            test_size=_test_size,
             random_state=random_state,
             stratify=stratify,
             shuffle=shuffle,
@@ -59,4 +58,40 @@ def split_dataset(
     else:
         validate = validate_test
         test = None
+    print(
+        f"Split as train: {train.shape[0]} ({train_size*100}%), "
+        f"validate: {validate.shape[0]} ({validate_size*100}%), "
+        f"test: {test.shape[0] if test is not None else None} ({test_size*100 if test_size is not None else None}%)"
+    )
     return train, validate, test
+
+
+def flatten(l):
+    return [item for sublist in l for item in sublist]
+
+
+def build_xy(
+    *dfs,
+    y_columns: list[str] | str,
+    xy_columns: list[str] | None = None,
+    drop_columns: list[str] | None = None,
+):
+    XX = []
+    yy = []
+    if isinstance(y_columns, str):
+        y_columns = [y_columns]
+    if drop_columns is not None and y_columns is not None:
+        drop_columns = list(drop_columns) + y_columns
+    else:
+        drop_columns = y_columns
+    for df in dfs:
+        df = df.copy()
+        y = df[y_columns].values.ravel()
+        X = df.drop(columns=drop_columns)
+        if xy_columns is not None:
+            for column in xy_columns:
+                y[column] = X[column]
+
+        XX.append(X)
+        yy.append(y)
+    return flatten([(X, y) for X, y in zip(XX, yy)])
