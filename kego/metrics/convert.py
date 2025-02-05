ed4c10df430a091a1befea050def7bcbe7334319
Kristian Ehlert <kristianehlert@gmail.com>
2025-02-05 13:26:55 -0800

    refactor, including functions

--- kego/metrics/convert.py
+++ kego/metrics/convert.py
@@ -1,5 +1,6 @@
 from typing import Any
 
+import numpy as np
 import pandas as pd
 
 from kego.columns import assert_missing_columns
@@ -17,6 +18,41 @@ def from_prediction_to_solution_submission(
     required_columns: list[str] | None = None,
     dummy_column_name_value: dict[str, Any] | None = None,
 ):
+    y_true, submission = create_submission(
+        y_true=y_true,
+        y_prediction=y_prediction,
+        X_prediction=X_prediction,
+        model=model,
+        target_column=target_column,
+    )
+    solution, submission, X_prediction = (
+        retain_df(y_true),
+        retain_df(submission),
+        retain_df(X_prediction),
+    )
+    if required_columns is not None:
+        for column in required_columns:
+            solution[column] = X_prediction[column]
+
+    add_dummy_columns(dummy_column_name_value, submission, solution)
+
+    submission = submission.rename(columns={target_column: prediction_column})
+    assert len(solution) == len(submission)
+    submission.insert(0, row_id_column_name, range(len(submission)))
+    solution.insert(0, row_id_column_name, range(len(solution)))
+    return solution, submission
+
+
+def add_dummy_columns(dummy_column_name_value, submission, solution):
+    if dummy_column_name_value is not None:
+        for dummy_column_key, dummy_column_value in dummy_column_name_value.items():
+            solution[dummy_column_key], submission[dummy_column_key] = (
+                dummy_column_value,
+                dummy_column_value,
+            )
+
+
+def create_submission(y_true, y_prediction, X_prediction, model, target_column):
     if y_prediction is None and X_prediction is None:
         raise ValueError(f"Need to provide {X_prediction} or {y_prediction}")
     if y_prediction is None and X_prediction is not None:
@@ -27,24 +63,10 @@ def from_prediction_to_solution_submission(
         else:
             prediction_values = model.predict(X_prediction.copy())
             y_prediction = pd.DataFrame(data={target_column: prediction_values})
+    elif y_prediction is not None:
+        assert (
+            target_column in y_prediction
+        ), f"{target_column=} not found in `y_prediction`."
     if not isinstance(y_true, pd.DataFrame):
-        y_true = pd.DataFrame(data={target_column: y_true})
-    solution, submission, X_prediction = (
-        retain_df(y_true),
-        retain_df(y_prediction),
-        retain_df(X_prediction),
-    )
-    if required_columns is not None:
-        for column in required_columns:
-            solution[column] = X_prediction[column]
-    if dummy_column_name_value is not None:
-        for dummy_column_key, dummy_column_value in dummy_column_name_value.items():
-            solution[dummy_column_key], submission[dummy_column_key] = (
-                dummy_column_value,
-                dummy_column_value,
-            )
-    submission = submission.rename(columns={target_column: prediction_column})
-    assert len(solution) == len(submission)
-    submission.insert(0, row_id_column_name, range(len(submission)))
-    solution.insert(0, row_id_column_name, range(len(solution)))
-    return solution, submission
+        y_true = pd.DataFrame(data={target_column: np.array(y_true)})
+    return y_true, y_prediction
