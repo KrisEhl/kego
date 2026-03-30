0612a709ac496d367e40af7b901fd11390adf5a9
kristian <kristian.ehlert@exxeta.com>
2026-03-30 10:13:00 +0200

    feat: run qunatum featuers in notebook

--- competitions/playground/quantum/reproduce-connection.py
+++ competitions/playground/quantum/reproduce-connection.py
@@ -7,13 +7,17 @@ from planqk.api.client import PlanqkApiClient
 
 np.random.seed(42)
 X_train = pd.DataFrame(
-    {"f0": np.random.randn(5), "f1": np.random.randn(5), "f2": np.random.randn(5)}
+    {"f0": np.random.randn(20), "f1": np.random.randn(20), "f2": np.random.randn(20)}
 )
-y_train = pd.DataFrame({"target": [0, 1, 0, 1, 0]})
+y_train = pd.DataFrame(
+    {"target": [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1]}
+)
 X_test = pd.DataFrame(
-    {"f0": np.random.randn(5), "f1": np.random.randn(5), "f2": np.random.randn(5)}
+    {"f0": np.random.randn(20), "f1": np.random.randn(20), "f2": np.random.randn(20)}
 )
-y_test = pd.DataFrame({"target": [1, 0, 1, 0, 1]})
+y_test = pd.DataFrame(
+    {"target": [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]}
+)
 
 dataset = {
     "training_tabular_data": X_train.to_dict(),
@@ -45,12 +49,10 @@ client = PlanqkServiceClient(
 )
 
 service_input = {
-    "data": {
-        "input_data_pool": {"id": input_dp.id, "ref": "DATAPOOL"},
-        "output_data_pool": {"id": output_dp.id, "ref": "DATAPOOL"},
-        "num_shots": 100,
-        "num_runs": 1,
-    }
+    "input_data_pool": {"id": input_dp.id, "ref": "DATAPOOL"},
+    "output_data_pool": {"id": output_dp.id, "ref": "DATAPOOL"},
+    "num_shots": 100,
+    "num_runs": 1,
 }
 
 execution = client.run(request=service_input)
